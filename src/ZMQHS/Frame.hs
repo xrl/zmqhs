cdab05faf8bac32cb38253d0d1b7333bfa9cb243
Xavier Lange <xrlange@gmail.com>
2011-10-21 01:47:35 -0700

    My parser is coming together. Still fighting the type checker. At least I can create bytestrings\!

--- src/ZMQHS/Frame.hs
+++ src/ZMQHS/Frame.hs
@@ -1,14 +1,37 @@
+-- Copyright:  Xavier Lange, 2011
+-- License:    BSD
+
+-- From the RFC:
+--more-frame  = length more body
+--final-frame = length final body
+--length      = OCTET / (%xFF 8OCTET)
+--more        = %x01
+--final       = %x00
+--body        = *OCTET
+
 module ZMQHS.Frame(
     parse
 )
 where
 import qualified Data.ByteString.Char8 as B
 --import qualified Data.Binary.Strict.Get as G
-import qualified Data.Binary.Get as G
-import qualified Data.Binary.Put as P
-import           Data.Bits
+--import qualified Data.Binary.Get as G
+import qualified Data.Binary.Strict.IncrementalGet as G
+--import qualified Data.Binary.Put as P
+--import           Data.Bits
 import qualified Data.Binary.Strict.BitGet as BG
 
+data FrameState = FINAL | MORE | ERROR
+
+parser = do
+    frame_length <- G.getWord8
+    state_val    <- G.getWord8
+    body         <- G.getByteString (fromIntegral frame_length)
+    return (frame_length, frame_state state_val, body)
+
 parse = do
-    id <- G.getWord16be
-    return id
+    G.runGet parser
+
+frame_state 0x00      = FINAL
+frame_state 0x01      = MORE
+frame_state otherwise = ERROR
--- test/frame/TestParse.hs
+++ test/frame/TestParse.hs
@@ -1,15 +1,13 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 import Data.ByteString.Char8 as B
 import Data.Binary.Get as G
 import Data.Binary.Put as P
-import ZMQHS.Frame ()
+import ZMQHS.Frame (parse)
 
 test_data = do
-	P.putWord8 0x01
-
-parse = do
-    id <- G.getWord16be
-    return id
+	return B.pack "\000\000\000"
 
 main = do
+	a_parsed_frame <- ZMQHS.Frame.parse test_data
 	Prelude.putStrLn "hi"
 	
