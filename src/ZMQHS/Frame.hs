c660a8ac09b1ed76f57d07102590483787711390
Xavier Lange <xrlange@gmail.com>
2011-10-22 23:39:13 -0700

    Moving to attoparsec\!

--- src/ZMQHS/Frame.hs
+++ src/ZMQHS/Frame.hs
@@ -9,29 +9,38 @@
 --final       = %x00
 --body        = *OCTET
 
-module ZMQHS.Frame(
-    parse
-)
+module ZMQHS.Frame
 where
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as B
 --import qualified Data.Binary.Strict.Get as G
 --import qualified Data.Binary.Get as G
-import qualified Data.Binary.Strict.IncrementalGet as G
+--import qualified Data.Binary.Strict.IncrementalGet as G
+import qualified Data.Attoparsec as AP
 --import qualified Data.Binary.Put as P
 --import           Data.Bits
-import qualified Data.Binary.Strict.BitGet as BG
+--import qualified Data.Binary.Strict.BitGet as BG
 
 data FrameState = FINAL | MORE | ERROR
 
 parser = do
-    frame_length <- G.getWord8
-    state_val    <- G.getWord8
-    body         <- G.getByteString (fromIntegral frame_length)
-    return (frame_length, frame_state state_val, body)
+	frame_length <- AP.anyWord8
+	state_val    <- AP.anyWord8
+	body         <- AP.take (fromIntegral frame_length)
+	AP.atEnd
+	return (frame_length, frame_state state_val, body)
+    --frame_length <- G.getWord8
+    --state_val    <- G.getWord8
+    --body         <- G.getByteString (fromIntegral frame_length)
+    --return (frame_length, frame_state state_val, body)
 
 parse = do
-    G.runGet parser
+    AP.parseTest parser
 
 frame_state 0x00      = FINAL
 frame_state 0x01      = MORE
 frame_state otherwise = ERROR
+
+instance Show FrameState where
+	show FINAL = "FINAL"
+	show MORE  = "MORE"
+	show ERROR = "ERROR"
--- test/frame/TestParse.hs
+++ test/frame/TestParse.hs
@@ -1,13 +1,8 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
-import Data.ByteString.Char8 as B
-import Data.Binary.Get as G
-import Data.Binary.Put as P
-import ZMQHS.Frame (parse)
 
-test_data = do
-	return B.pack "\000\000\000"
+import qualified Data.ByteString as B (pack)
+
+test_data = B.pack [0,0,0]
 
 main = do
-	a_parsed_frame <- ZMQHS.Frame.parse test_data
-	Prelude.putStrLn "hi"
-	
+    Prelude.putStrLn "hi"
