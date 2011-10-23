4f30a77595b8678a158e9a5b230fb8a37cecb4ea
Xavier Lange <xrlange@gmail.com>
2011-10-23 02:54:45 -0700

    Now I can see myself parsing just a little more data. Will be looking at applicative to sugar it up.

--- src/ZMQHS/Frame.hs
+++ src/ZMQHS/Frame.hs
@@ -12,13 +12,7 @@
 module ZMQHS.Frame
 where
 import qualified Data.ByteString as B
---import qualified Data.Binary.Strict.Get as G
---import qualified Data.Binary.Get as G
---import qualified Data.Binary.Strict.IncrementalGet as G
 import qualified Data.Attoparsec as AP
---import qualified Data.Binary.Put as P
---import           Data.Bits
---import qualified Data.Binary.Strict.BitGet as BG
 
 data FrameState = FINAL | MORE | ERROR
 
@@ -26,12 +20,7 @@ parser = do
 	frame_length <- AP.anyWord8
 	state_val    <- AP.anyWord8
 	body         <- AP.take (fromIntegral frame_length)
-	AP.atEnd
 	return (frame_length, frame_state state_val, body)
-    --frame_length <- G.getWord8
-    --state_val    <- G.getWord8
-    --body         <- G.getByteString (fromIntegral frame_length)
-    --return (frame_length, frame_state state_val, body)
 
 parse = do
     AP.parseTest parser
--- test/frame/TestParse.hs
+++ test/frame/TestParse.hs
@@ -1,8 +1,24 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 import qualified Data.ByteString as B (pack)
+import qualified Data.Attoparsec as AP
+import qualified ZMQHS.Frame     as ZF
 
-test_data = B.pack [0,0,0]
+test_one_complete   = B.pack [1,0,65]
+test_two_incomplete = B.pack [1,0]
+test_two_rest       = B.pack [66]
 
 main = do
-    Prelude.putStrLn "hi"
+	AP.parseTest ZF.parser test_one_complete
+	AP.parseTest ZF.parser test_two_incomplete
+	case (AP.parse ZF.parser test_two_incomplete) of
+		AP.Partial cont -> case (cont test_two_rest) of
+							AP.Done _ val        -> putStrLn ("Got val: " ++ show val)
+							AP.Partial more      -> putStrLn ("Need more")
+							AP.Fail dat ctxs msg -> putStrLn (show msg)
+	putStrLn "hi"
+    --case AP.parse ZF.parser test_data of
+    --	AP.Partial moar      -> putStrLn "Need more"
+    --	AP.Done bs retval    -> putStrLn $ show retval
+    --	AP.Fail dat ctxs msg -> putStrLn $ show msg
+    --where parser = ZF.parser
