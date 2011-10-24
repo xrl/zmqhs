b65a7237b5af17666fee6ef5a250f154cef32260
Xavier Lange <xrlange@gmail.com>
2011-10-24 12:01:52 -0700

    Now parsing with error messages. Removed the ERROR state value.

--- src/ZMQHS/Frame.hs
+++ src/ZMQHS/Frame.hs
@@ -11,25 +11,28 @@
 
 module ZMQHS.Frame
 where
+import Control.Monad (guard)
 import qualified Data.ByteString as B
 import qualified Data.Attoparsec as AP
 
 data FrameState = FINAL | MORE | ERROR
 
+--frame_length <- AP.anyWord8
+
 parser = do
-	frame_length <- AP.anyWord8
-	state_val    <- AP.anyWord8
-	body         <- AP.take (fromIntegral frame_length)
-	return (frame_length, frame_state state_val, body)
+    frame_length <- AP.anyWord8
+    state_val    <- AP.anyWord8
+    guard(state_val == 0x00 || state_val == 0x01) AP.<?> "State must be either MORE or FINAL"
+    body         <- AP.take (fromIntegral frame_length)
+    return (frame_length, frame_state state_val, body)
 
 parse = do
     AP.parseTest parser
 
 frame_state 0x00      = FINAL
 frame_state 0x01      = MORE
-frame_state otherwise = ERROR
+--frame_state otherwise = "you should never see this!"
 
 instance Show FrameState where
-	show FINAL = "FINAL"
-	show MORE  = "MORE"
-	show ERROR = "ERROR"
+    show FINAL = "FINAL"
+    show MORE  = "MORE"
--- test/frame/TestParse.hs
+++ test/frame/TestParse.hs
@@ -4,21 +4,21 @@ import qualified Data.ByteString as B (pack)
 import qualified Data.Attoparsec as AP
 import qualified ZMQHS.Frame     as ZF
 
-test_one_complete   = B.pack [1,0,65]
+test_one_complete   = B.pack [1,4,65]
 test_two_incomplete = B.pack [1,0]
 test_two_rest       = B.pack [66]
 
 main = do
-	AP.parseTest ZF.parser test_one_complete
-	AP.parseTest ZF.parser test_two_incomplete
-	case (AP.parse ZF.parser test_two_incomplete) of
-		AP.Partial cont -> case (cont test_two_rest) of
-							AP.Done _ val        -> putStrLn ("Got val: " ++ show val)
-							AP.Partial more      -> putStrLn ("Need more")
-							AP.Fail dat ctxs msg -> putStrLn (show msg)
-	putStrLn "hi"
+    AP.parseTest ZF.parser test_one_complete
+    AP.parseTest ZF.parser test_two_incomplete
+    case (AP.parse ZF.parser test_two_incomplete) of
+        AP.Partial cont -> case (cont test_two_rest) of
+                            AP.Done _ val        -> putStrLn ("Got val: " ++ show val)
+                            AP.Partial more      -> putStrLn ("Need more")
+                            AP.Fail dat ctxs msg -> putStrLn (show msg)
+    
     --case AP.parse ZF.parser test_data of
-    --	AP.Partial moar      -> putStrLn "Need more"
-    --	AP.Done bs retval    -> putStrLn $ show retval
-    --	AP.Fail dat ctxs msg -> putStrLn $ show msg
+    --  AP.Partial moar      -> putStrLn "Need more"
+    --  AP.Done bs retval    -> putStrLn $ show retval
+    --  AP.Fail dat ctxs msg -> putStrLn $ show msg
     --where parser = ZF.parser
