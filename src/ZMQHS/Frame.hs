3c450c62bac0df6a975ad5427257770580ffb7e2
Xavier Lange <xrlange@gmail.com>
2011-10-29 22:01:50 -0700

    Code cleaned up with attoparsec-binary

--- src/ZMQHS/Frame.hs
+++ src/ZMQHS/Frame.hs
@@ -13,13 +13,9 @@ module ZMQHS.Frame
 where
 import Control.Monad (guard)
 import Control.Applicative hiding (empty)
-import qualified Data.ByteString as B (ByteString, pack, unpack)
 import           Data.Word (Word8, Word64)
 import qualified Data.Attoparsec as AP
 import qualified Data.Attoparsec.Binary as APB
-import qualified Data.Binary.Get as G
-import           Data.Bits ((.|.), shiftL)
-import qualified Data.List as L
 
 data FrameCont = FINAL | MORE | BADCONT
     deriving (Show, Eq)
@@ -35,25 +31,11 @@ get_fc = do
     guard((frame_cont raw_cont) /= BADCONT) AP.<?> "State must be either MORE or FINAL"
     return raw_cont
 
---to_word64 = L.foldl' (\w b -> (shiftL w 8) .|. fromIntegral b) 0 . B.unpack
-
---get_word64 = do
---    bs <- AP.take 8
---    return (G.runGet G.getWord64be bs)
---case bs of
---    a -> G.runGet G.getWord64be bs
---    AP.Partial _ -> error "what"
---    AP.Fail _ _ _ -> error "what"
-
 parser = do
     frame_length <- AP.anyWord8
     frame_size <- case frame_length of
-        --0xFF       ->  Jumbo <$> (AP.take 8)
         0xFF       ->  Jumbo <$> APB.anyWord64be
         otherwise  ->  return (Small otherwise)
     fc <- get_fc
     bs <- AP.take 8
-    --case frame_size of
-    --    Small len -> return AP.take $ fromIntegral len
-    --    Jumbo len -> return AP.take $ fromIntegral len
     return (frame_size, fc, bs)
