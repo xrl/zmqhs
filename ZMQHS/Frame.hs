bb0fe3259b70a32a3be4b03e32b625313c011c58
Xavier Lange <xrlange@gmail.com>
2012-01-27 21:21:27 -0800

    Fixed off-by-one parsing error, added mystery placeholder

--- ZMQHS/Frame.hs
+++ ZMQHS/Frame.hs
@@ -11,17 +11,19 @@
 
 module ZMQHS.Frame
 where
-import Control.Monad (guard)
+import Control.Monad
 import Control.Applicative hiding (empty)
 import           Data.Word (Word8, Word64)
 import qualified Data.Attoparsec as AP
 import qualified Data.Attoparsec.Binary as APB
+import qualified Data.Binary.Put as P
 import Debug.Trace
 
-data FrameCont = FINAL | MORE | BADCONT
+data FrameCont = FINAL | MORE | MYSTERIOUS | BADCONT
     deriving (Show, Eq)
 frame_cont 0x00      = FINAL
 frame_cont 0x01      = MORE
+frame_cont 0x7E      = MYSTERIOUS
 frame_cont otherwise = BADCONT
 
 get_fc = do
@@ -30,10 +32,28 @@ get_fc = do
     return raw_cont
 
 parser = do
-    first_byte <- fromIntegral <$> AP.anyWord8
-    frame_size <- case first_byte of
+    first_byte    <- fromIntegral <$> AP.anyWord8
+    rest_of_frame <- case first_byte of
         0xFF ->  fromIntegral <$> APB.anyWord64be
         _    ->  return first_byte
     fc <- get_fc
-    body <- AP.take frame_size
-    return (frame_size, fc, body)
+    let payload_size = rest_of_frame - 1
+    payload <- AP.take payload_size
+    return (payload_size, fc, payload)
+
+handshake_response_generator = do
+    P.putWord8 0x01
+    P.putWord8 0x7E
+handshake_response = do
+    P.runPut handshake_response_generator
+
+--generator (byte:[]) = do
+--    P.putWord8 byte
+--generator bytes =
+--[(P.putWord8)] `ap` bytes
+--liftM (P.putWord8) bytes
+--mapM (P.putWord8) bytes
+--sequence_ (P.putWord8) bytes
+--P.putWord8 =<< bytes
+--P.putWord8 byte0 >>
+--P.putWord8 byte1
