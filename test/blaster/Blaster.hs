101c3657d697a27668a088c3f73da4d8df80f074
Xavier Lange <xrlange@gmail.com>
2012-06-17 15:48:05 +0200

    Blaster.hs now sends out repeated ZMQ messages

--- test/blaster/Blaster.hs
+++ test/blaster/Blaster.hs
@@ -21,23 +21,26 @@ liftMaybeT :: Monad m => Maybe a -> MaybeT m a
 liftMaybeT  = MaybeT . return
 
 data Mode = Pub | Sub deriving (Typeable, Data, Show)
-data CmdOp = CmdOp {mode_      :: Mode,
-                    target_    :: String,
-                    identity_  :: String,
-                    payload_   :: String}
+data CmdOp = CmdOp {mode_        :: Mode,
+                    target_      :: String,
+                    identity_    :: String,
+                    payload_     :: String,
+                    repetitions_ :: Int}
      deriving (Typeable, Data, Show)
 data Op = Op {mode        :: Mode,
               target      :: String,
               target_spec :: Maybe Z.ConnSpec,
               identity    :: String,
-              payload     :: String}
+              payload     :: String,
+              repetitions :: Int}
      deriving (Show)
 
 operation = CmdOp {mode_        = enum [Pub &= help "Pub",
                                         Sub &= help "Sub"],
                    target_      = "" &= typ "URI" &= help "Target URI spec",
                    identity_    = "anonymous" &= typ "ID" &= help "Use name on all messages",
-                   payload_     = "TESTPAYLOAD" &= typ "BYTES" &= help "Message payload"}
+                   payload_     = "TESTPAYLOAD" &= typ "BYTES" &= help "Message payload",
+                   repetitions_ = 1 &= typ "REPEAT" &= help "Repititions"}
                 &= program "blaster"
                 &= summary "One stop shop for using the ZMQHS library to poke around your ZMQ network"
                 -- &= verbosity
@@ -51,21 +54,24 @@ main = do
     Nothing        -> putStrLn "Target spec must be a valid spec"
   return ()
 
-op_from_cmd_op CmdOp{mode_=m,target_=t,identity_=i,payload_=p} =
+op_from_cmd_op CmdOp{mode_=m,target_=t,identity_=i,payload_=p,repetitions_=r} =
   Op {mode        = m,
       target      = t,
       target_spec = Z.spec t,
       identity    = i,
-      payload     = p}
+      payload     = p,
+      repetitions = r}
 
 to_ident :: String -> Z.Identity
 to_ident  "anonymous" = Z.Anonymous
 to_ident  name        = Z.Named (pack name)
 
 exec :: Op -> IO ()
-exec Op {mode=Pub, target_spec=Just target_spec, payload=payload, identity=identity} = do
-  -- Z.Client sock <- Z.connect target_spec typed_id
-  -- putStrLn $ "sock: " ++ (show sock)
-  putStrLn "hi, I compile"
+exec Op {mode=Sub, target_spec=Just target_spec, payload=payload, identity=identity} = do
   return ()
-  where typed_id = to_ident identity
+exec Op {mode=Pub, target_spec=Just target_spec, payload=payload, identity=identity,repetitions=rep} = do
+  conn <- Z.client target_spec (to_ident identity)
+  foldr (>>) (return ()) (take rep $ repeat (Z.sendMessage conn (Z.Message [pack payload])))
+  return ()
+exec Op {target_spec=Nothing} = do
+  putStrLn "Sorry, please enter a valid target specification (e.g., \"tcp://localhost:7890\""
