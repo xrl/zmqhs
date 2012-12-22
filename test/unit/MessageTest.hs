56e55e9f9055105b18903d47d58ae0f032550c00
Xavier Lange <xrlange@gmail.com>
2012-12-22 15:01:00 -0800

    Specs working

--- test/unit/MessageTest.hs
+++ test/unit/MessageTest.hs
@@ -1,45 +1,50 @@
-import           Test.HUnit
+module Main (main)
+where
+
+import           Debug.Trace (trace)
+
 import           Test.Hspec
-
-import Control.Exception (fromException)
-import Control.Monad.Trans
-
-import Debug.Trace (trace)
 
 import qualified ZMQHS as Z
 import qualified Data.Attoparsec.ByteString as AP
 
 import qualified Data.ByteString as B
 
-test_one_complete      = B.pack [1,1,65]
-test_two_incomplete    = B.pack [1,0]
-test_two_rest          = B.pack [67]
-test_three_complete    = B.pack [0xFF,1,0,0,0,0,0,0,0,1,67]
+--first_stage_handshake  = B.pack [0x01, 0x7E]
+--second_stage_handshake = B.pack [0x1d, 0x7e, 0x41, 0x53, 0x44, 0x46,
+--                                 0x41, 0x53, 0x44, 0x46, 0x41, 0x53,
+--                                 0x44, 0x46, 0x41, 0x53, 0x44, 0x46,
+--                                 0x41, 0x53, 0x44, 0x46, 0x41, 0x53,
+--                                 0x44, 0x46, 0x41, 0x53, 0x44, 0x46]
 
-first_stage_handshake  = B.pack [0x01, 0x7E]
-second_stage_handshake = B.pack [0x1d, 0x7e, 0x41, 0x53, 0x44, 0x46,
-                                 0x41, 0x53, 0x44, 0x46, 0x41, 0x53,
-                                 0x44, 0x46, 0x41, 0x53, 0x44, 0x46,
-                                 0x41, 0x53, 0x44, 0x46, 0x41, 0x53,
-                                 0x44, 0x46, 0x41, 0x53, 0x44, 0x46]
+examples :: [(String, B.ByteString, Z.Frame)]
+examples = [("complete with payload 'A'",   B.pack [2,0,65],    Z.FinalFrame $ B.pack [65]),
+            ("incomplete with payload 'A'", B.pack [2,1,65],    Z.MoreFrame  $ B.pack [65]),
+            ("first handshake",             B.pack [0x01,0x7E], Z.FinalFrame $ B.pack []  )]
 
-examples = [("Complete with payload 'A'",   B.pack [1,0,65], Z.FinalFrame $ B.pack [65]),
-            ("Incomplete with payload 'A'", B.pack [1,1,65], Z.MoreFrame  $ B.pack [65])]
-
+main :: IO ()
 main = do
-  hspec spec
--- https://github.com/snoyberg/conduit/blob/master/attoparsec-conduit/test/main.hs
-spec = do
-  describe "parsing frames" $ do
+  hspec $ describe "parsing frames" $ do
     context "the parser" $ do
-      mapM_ (\(msg,frame,expected) ->
-        it msg $ case AP.parse Z.frameParser frame of
-                  expected  -> True
-                  otherwise -> trace (show otherwise) False
-        ) examples
+      mapM_ itRunner examples
 
-        --case (AP.parse Z.frameParser test_one_complete) of
-        --  (AP.Done _ (Z.FinalFrame (payload))) -> True
-        --  (AP.Partial _) -> True
-        --  (AP.Fail _ _ _) -> False
-        --  otherwise -> trace (show otherwise) (False)
+itRunner :: (String,B.ByteString,Z.Frame) -> Spec
+itRunner (msg,frame,expected) = it msg (frameExample frame expected)
+
+frameExample :: B.ByteString -> Z.Frame -> Bool
+frameExample bytes expected = case AP.parse Z.frameParser bytes of
+    AP.Done leftover res -> res == expected && B.null leftover
+    _ -> False
+
+--it msg $ case AP.parse Z.frameParser frame == expected of
+--          True  -> True
+--          False -> trace ("Failed on " ++ show frame) False
+
+-- https://github.com/snoyberg/conduit/blob/master/attoparsec-conduit/test/main.hs
+
+
+--case (AP.parse Z.frameParser test_one_complete) of
+--  (AP.Done _ (Z.FinalFrame (payload))) -> True
+--  (AP.Partial _) -> True
+--  (AP.Fail _ _ _) -> False
+--  otherwise -> trace (show otherwise) (False)
