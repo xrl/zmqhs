04f04c03ef369e1022a8005a44634b027fc36b30
Xavier Lange <xrlange@gmail.com>
2012-12-26 16:15:07 -0800

    Message specs and cabal test works

--- test/unit/MessageTest.hs
+++ test/unit/MessageTest.hs
@@ -1,50 +1,57 @@
 module Main (main)
 where
 
-import           Debug.Trace (trace)
+-- nice example, using many of the same libraries, for writing specs
+-- https://github.com/snoyberg/conduit/blob/master/attoparsec-conduit/test/main.hs
 
-import           Test.Hspec
+-- import           Debug.Trace (trace)
+
+import           Test.Hspec (hspec, describe, it, shouldBe, shouldSatisfy, Spec, Expectation)
+import           Test.HUnit (assertFailure, assert)
 
 import qualified ZMQHS as Z
 import qualified Data.Attoparsec.ByteString as AP
 
-import qualified Data.ByteString as B
-
---first_stage_handshake  = B.pack [0x01, 0x7E]
---second_stage_handshake = B.pack [0x1d, 0x7e, 0x41, 0x53, 0x44, 0x46,
---                                 0x41, 0x53, 0x44, 0x46, 0x41, 0x53,
---                                 0x44, 0x46, 0x41, 0x53, 0x44, 0x46,
---                                 0x41, 0x53, 0x44, 0x46, 0x41, 0x53,
---                                 0x44, 0x46, 0x41, 0x53, 0x44, 0x46]
-
-examples :: [(String, B.ByteString, Z.Frame)]
-examples = [("complete with payload 'A'",   B.pack [2,0,65],    Z.FinalFrame $ B.pack [65]),
-            ("incomplete with payload 'A'", B.pack [2,1,65],    Z.MoreFrame  $ B.pack [65]),
-            ("first handshake",             B.pack [0x01,0x7E], Z.FinalFrame $ B.pack []  )]
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Char8 as B8
 
 main :: IO ()
 main = do
-  hspec $ describe "parsing frames" $ do
-    context "the parser" $ do
-      mapM_ itRunner examples
+  hspec $ (describe "complete frame parsing" $ do
+    mapM_ completeFrameSpec completeParses) >>
+          (describe "messages" $ do
+    mapM_ messageSpec completeMessages)
 
-itRunner :: (String,B.ByteString,Z.Frame) -> Spec
-itRunner (msg,frame,expected) = it msg (frameExample frame expected)
+--------------------------------
+-- MESSAGE SPECS
+--------------------------------
+messageSpec :: (String,[B.ByteString],Z.Message) -> Spec
+messageSpec (msg,frames,expected) = it msg (messageExample frames expected)
 
-frameExample :: B.ByteString -> Z.Frame -> Bool
+messageExample :: [B.ByteString] -> Z.Message -> Expectation
+messageExample frames expected = case AP.parse Z.getMessage (B.concat frames) of
+  AP.Done _ _   -> assert True
+  AP.Partial _  -> assertFailure "should not get a partial result"
+  AP.Fail _ _ _ -> assertFailure "should not get a failure from parsing"
+
+completeMessages :: [(String,[B.ByteString],Z.Message)]
+completeMessages =  [("one part",[B.pack [2,0,65]], Z.Message [B8.pack "A"])]
+
+--------------------------------
+-- FRAME SPECS
+--------------------------------
+completeFrameSpec :: (String,B.ByteString,Z.Frame) -> Spec
+completeFrameSpec (msg,frame,expected) = it msg (frameExample frame expected)
+
+frameExample :: B.ByteString -> Z.Frame -> Expectation
 frameExample bytes expected = case AP.parse Z.frameParser bytes of
-    AP.Done leftover res -> res == expected && B.null leftover
-    _ -> False
+    AP.Done leftover res -> (res `shouldBe` expected) >> (leftover `shouldSatisfy` B.null)
+    AP.Partial _ -> assertFailure "should not get a partial result"
+    AP.Fail _ _ _ -> assertFailure "should not get a failure from parsing"
 
---it msg $ case AP.parse Z.frameParser frame == expected of
---          True  -> True
---          False -> trace ("Failed on " ++ show frame) False
-
--- https://github.com/snoyberg/conduit/blob/master/attoparsec-conduit/test/main.hs
-
-
---case (AP.parse Z.frameParser test_one_complete) of
---  (AP.Done _ (Z.FinalFrame (payload))) -> True
---  (AP.Partial _) -> True
---  (AP.Fail _ _ _) -> False
---  otherwise -> trace (show otherwise) (False)
+completeParses :: [(String, B.ByteString, Z.Frame)]
+completeParses = [("complete   with payload",  B.pack [2,0,65],      Z.FinalFrame $ B8.pack "A"),
+                  ("incomplete with payload",  B.pack [2,1,65],      Z.MoreFrame  $ B8.pack "A"),
+                  ("handshake  w/o  identity", B.pack [0x01,0x7E],   Z.FinalFrame $ B8.pack "" ),
+                  ("handshake  w/   identity", handshake_w_identity, Z.FinalFrame $ B8.pack "ASDFASDFASDFASDFASDFASDFASDF")]
+                where handshake_w_identity = B8.concat [B.pack [29,0x7E] , B8.pack "ASDFASDFASDFASDFASDFASDFASDF"]
